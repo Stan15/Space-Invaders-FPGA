da4e7fa652f10d500bc55efd4176a09323ad6b7b
stan15 <stanihe1901@gmail.com>
2022-03-27 20:19:52 -0400

    fixed merging issues

--- color_mapper.sv
+++ color_mapper.sv
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 module color_mapper(input clk, input [3:0] color_code, output [11:0] color_value);
-=======
-module color_mapper(input clk, input [3:0] color_code, output [11:0] color);
->>>>>>> 1259f1f3171c0e05b3369a4b55a9e469e8c3605c
 	localparam INIT_F = "color_map.mem";
 	
 	reg [15:0]memory[15:0];
@@ -14,10 +10,6 @@ module color_mapper(input clk, input [3:0] color_code, output [11:0] color);
 	end
 	
 	always_ff @(posedge clk) begin
-<<<<<<< HEAD
 		color_value <= memory[color_code];
-=======
-		color <= memory[color_code];
->>>>>>> 1259f1f3171c0e05b3369a4b55a9e469e8c3605c
 	end
 endmodule
